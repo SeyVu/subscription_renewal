40d98e32d19dc84a3fa038b1c3d014b2d83e2e55
HarshaG <harsha.gopianandan@seyvu.com>
2015-12-12 11:08:35 -0800

    Created new file ensemble_models and moved corresponding functions from baseline_models to there

--- baseline_models.py
+++ baseline_models.py
@@ -2,8 +2,6 @@
 #  Description: Low-level functions for running classification models and model ensembles
 #
 #########################################################################################################
-from __future__ import division  # Used in matplotlib
-
 # RandomForest is default model specified in some methods. Might not be used by high-level functions
 from sklearn.ensemble import RandomForestClassifier as RandomForest
 
@@ -228,7 +226,7 @@ def accuracy(y_true, y_pred):
 # If run_prob_predictions is False, we rely on the model to give classified outputs.
 # If true, then model gives probabilities as outputs and we use a threshold to classify them into
 # different classes. Currently probability predictions only support 2 classes
-def run_cv(x, y, run_prob_predictions=False, clf_class=RandomForest, **kwargs):
+def run_cv(x, y, run_prob_predictions=False, return_yprob=False, clf_class=RandomForest, **kwargs):
     # Construct a kfolds object
     kf = KFold(len(y), n_folds=5, shuffle=True)
 
@@ -280,14 +278,21 @@ def run_cv(x, y, run_prob_predictions=False, clf_class=RandomForest, **kwargs):
                 y_pred[idx] = 1
                 # print y_prob
 
-    return y_pred
+    if not run_prob_predictions and return_yprob:
+        raise ValueError("Invalid combination - cannot return yprob when run_prob_predictions is False!")
+
+    if return_yprob:
+        return y_prob
+    else:
+        return y_pred
 
 
 # Test on different dataset. Classify users into if they'll churn or no
 # If run_prob_predictions is False, we rely on the model to give classified outputs.
 # If true, then model gives probabilities as outputs and we use a threshold to classify them into
 # different classes. Currently probability predictions only support 2 classes
-def run_test(x_train, y_train, x_test, run_prob_predictions=False, clf_class=RandomForest, **kwargs):
+def run_test(x_train, y_train, x_test, run_prob_predictions=False, return_yprob=False,
+             clf_class=RandomForest, **kwargs):
     y_pred = np.zeros((len(x_test), 1), dtype=int)
 
     # Initialize y_prob for predicting probabilities
@@ -338,7 +343,13 @@ def run_test(x_train, y_train, x_test, run_prob_predictions=False, clf_class=Ran
 
     y_pred = np.array(y_pred)
 
-    return y_pred
+    if not run_prob_predictions and return_yprob:
+        raise ValueError("Invalid combination - cannot return yprob when run_prob_predictions is False!")
+
+    if return_yprob:
+        return y_prob
+    else:
+        return y_pred
 
 
 # Run k-fold cross-validation. Classify users into if they'll churn or no
@@ -434,66 +445,5 @@ def compare_prob_predictions(cv_0_test_1, x, y, x_test, clf_class, **kwargs):
     print counts
     # print ("Num_wrong_predictions")
     # print (1.0 - counts.icol(0)) * counts.icol(1) * counts.icol(2)
-
-
-# Use multiple models (minimum 2) to create an ensemble
-# Use majority voting to predict classes of new ensemble. For even number of models, split = majority!
-def models_ensemble(input_features, output, model_names, model_parameters, run_cv_flag=False, num_model_iterations=1,
-                    plot_learning_curve=False, run_prob_predictions=True):
-    # Check if a minimum of 3 models are there
-    if len(model_names) < 2:
-        raise ValueError("Need a minimum of 2 models to do an ensemble")
-
-    actual_output_values = dict()
-    predicted_output_values = dict()
-
-    num_of_models = len(model_names)
-
-    # Get actual and predicted values for each model
-    for idx in range(num_of_models):
-        model_key = "model{:d}".format(idx)
-        logger.info(sf.Color.BOLD + sf.Color.GREEN + "\nRunning Model {:s}".format(model_names[model_key]) +
-                    sf.Color.END)
-        # Append to dictionary with dynamically created key names above
-        [actual_output_values[model_key], predicted_output_values[model_key]] = \
-            run_models_wrapper(x=input_features, y=output, run_cv_flag=run_cv_flag,
-                               num_model_iterations=num_model_iterations,
-                               plot_learning_curve=plot_learning_curve, run_prob_predictions=run_prob_predictions,
-                               clf_class=model_names[model_key], **model_parameters[model_key])
-
-        # accuracy(actual_output_values[actual_output_name], predicted_output_values[predicted_output_name])
-
-    y_predicted_ensemble = predicted_output_values['model0'].copy()
-
-    # # Create ensemble prediction using majority voting scheme
-    for sample in np.ndindex(predicted_output_values['model0'].shape):
-        y_predicted_sum = 0  # Reset for every sample
-        for actual_key_name in actual_output_values.iterkeys():
-            if predicted_output_values[actual_key_name][sample]:
-                y_predicted_sum += 1
-
-        # Need to have either numerator or denominator in round() as float to roundup
-        if y_predicted_sum >= round(num_of_models / 2.0):
-            y_predicted_ensemble[sample] = 1
-        else:
-            y_predicted_ensemble[sample] = 0
-
-    accuracy_value = accuracy(actual_output_values['model0'], y_predicted_ensemble)
-
-    beta = 2.0
-
-    prec_recall = precision_recall_fscore_support(y_true=actual_output_values['model0'], y_pred=y_predicted_ensemble,
-                                                  beta=beta, average='binary')
-
-    # Log Accuracy and precision / recall values for the ensemble
-    logger.info(sf.Color.BOLD + sf.Color.DARKCYAN + "\nEnsemble output for test data" + sf.Color.END)
-
-    logger.info(
-        sf.Color.BOLD + sf.Color.DARKCYAN + "\nAccuracy {:.2f}".format(accuracy_value * 100) +
-        sf.Color.END)
-
-    logger.info(
-        sf.Color.BOLD + sf.Color.DARKCYAN + "\nPrecision {:.2f} Recall {:.2f} Fbeta-score {:.2f}".format(
-            prec_recall[0] * 100, prec_recall[1] * 100, prec_recall[2] * 100) + sf.Color.END)
 
 ##################################################################################################################
